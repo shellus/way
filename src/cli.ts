78f871d2c47e603b47826ae4b7b68b83fe0fce19
GeJiaXiang <353358601@qq.com>
2026-05-04 16:33:56 +0800

    chore: 发布 v0.6.1

--- src/cli.ts
+++ src/cli.ts
@@ -11,7 +11,7 @@ const program = new Command()
 
 program
   .name('way')
-  .version('0.6.0')
+  .version('0.6.1')
   .description('策略备份工具 - 基于 restic 的策略封装')
   .option('--remote <name>', '指定仓库', 'default')
   .addHelpText('after', `
